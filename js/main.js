4f4f033100825d1e11cb16f3772c8a010b637644
Natalia Paszkowska <npaszkowska@gmail.com>
2017-07-02 16:31:36 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 $(function() {
- 
-  $(window).scroll(function () {
+
+$("a[href*='#']").click(function() {
+	$('body').animate({
+		scrollTop: $(this.hash).offset().top - 50 }, 500
+	);
+});
+
+$(window).scroll(function () {
 	if($(window).scrollTop() >=50) {
 		$('#main-nav').addClass('scroll');
 		$('#main-nav').css({'transition': 'all 0.3'});
